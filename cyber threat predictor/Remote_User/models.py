9280bcf5b14a33236f99b59b4a8c6e894d988789
Vamsi Krishna <104820101+VamsiKr1shna@users.noreply.github.com>
2024-10-04 21:29:24 +0530

    added models for Remote User

---
+++ cyber threat predictor/Remote_User/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+from django.db.models import CASCADE
+
+
+class ClientRegister_Model(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    password = models.CharField(max_length=10)
+    phoneno = models.CharField(max_length=10)
+    country = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+
+
+class detect_cyber_threat(models.Model):
+
+    pid= models.CharField(max_length=300)
+    ptime= models.CharField(max_length=300)
+    src_ip_address= models.CharField(max_length=300)
+    dst_ip_address= models.CharField(max_length=300)
+    frame_protos= models.CharField(max_length=300)
+    src_port= models.CharField(max_length=300)
+    dst_port= models.CharField(max_length=300)
+    bytes_trans= models.CharField(max_length=300)
+    protocol= models.CharField(max_length=300)
+    Date1= models.CharField(max_length=300)
+    Prediction= models.CharField(max_length=300)
+
+
+class detection_accuracy(models.Model):
+
+    names = models.CharField(max_length=300)
+    ratio = models.CharField(max_length=300)
+
+class detection_ratio(models.Model):
+
+    names = models.CharField(max_length=300)
+    ratio = models.CharField(max_length=300)
+
+
+
